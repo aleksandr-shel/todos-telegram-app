2d1e3f1fbce7dc93a121b414f517bb4d7812cba6
Aleksandr Shelukheev <60274747+aleksandr-shel@users.noreply.github.com>
2025-12-26 17:06:35 +0800

    store changes

--- frontend/src/common/store.js
+++ frontend/src/common/store.js
@@ -8,30 +8,30 @@ export const store={
     selectedGroup:null,
     setTasks: function(tasks){
         this.tasks = tasks
-        renderList(this.tasks, todoListDiv)
+        // renderList(this.tasks, todoListDiv)
     },
     deleteTask: function(id){
         this.tasks = this.tasks.filter(task => task.id !== id)
-        renderList(this.tasks, todoListDiv)
+        // renderList(this.tasks, todoListDiv)
     },
     addTask: function(task){
         this.tasks.unshift(task)
-        renderTask(task, todoListDiv, false)
+        // renderTask(task, todoListDiv, false)
     },
     updateTask: function(id, updTask){
         this.tasks = this.tasks.map(task => task.id === id ? updTask : task)
-        renderList(this.tasks, todoListDiv)
+        // renderList(this.tasks, todoListDiv)
     },
     setSelectedTask: function(task){
         this.selectedTask=task
     },
     setGroups: function(groups){
         this.groups = groups
-        renderGroups(this.groups)
+        // renderGroups(this.groups)
     }, 
     selectGroup: function(group){
         this.selectedGroup = group
-        loadOneGroup(this.selectedGroup.id)
+        // loadOneGroup(this.selectedGroup.id)
     },
 
 }
