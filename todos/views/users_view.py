f1704083d498d1b3065b11a37d44604f61e87e63
Aleksandr Shelukheev <60274747+aleksandr-shel@users.noreply.github.com>
2026-01-14 14:24:16 +0800

    l

--- todos/views/users_view.py
+++ todos/views/users_view.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from ..models import User
 
 class UsersView(APIView):
     
     # search users
     def get(self, request):
-        print(request.query_params)
-        return Response("dsd")
+        q=request.query_params.get('q','')
+        users=User.objects.filter(username__icontains=q).values('id','username','telegram_id')[:10]
+        return Response(users)
